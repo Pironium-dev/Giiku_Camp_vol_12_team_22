73ac7a01b2af224be40f1cbce4be756bc6bae5c0
mizuiro <22238287@edu.cc.saga-u.ac.jp>
2024-08-10 16:20:27 +0900

    _choiceの実装

--- Giiku_Camp_vol_12_team_22/Giiku_Camp_vol_12_team_22.py
+++ Giiku_Camp_vol_12_team_22/Giiku_Camp_vol_12_team_22.py
@@ -6,6 +6,11 @@ from rxconfig import config
 
 class State(rx.State):
     generated_name = ''
+
+    head_options = []
+    middle_options = []
+    tail_options = []
+
     def generate_team_name(self):
         if random.randint(0, 3) == 0:
             self.generated_name = self._choice(1) + self._choice(2)
@@ -14,7 +19,14 @@ class State(rx.State):
         print(self.generated_name) ## デバッグ用
     
     def _choice(self, index):
-        pass
+        if index == 0:
+            option = random.choice(self.head_options)
+        elif index == 1:
+            option = random.choice(self.middle_options)
+        else:
+            option = random.choice(self.tail_options)
+
+        return option
 
 
 def index() -> rx.Component:
