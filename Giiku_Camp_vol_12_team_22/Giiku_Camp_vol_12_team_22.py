2d6c0f283c3ab6f9a88e3f4bb96300cfbe24dc55
Pironium-dev <pirogurama@gmail.com>
2024-08-10 15:51:11 +0900

    ざっくりとした枠組み

--- Giiku_Camp_vol_12_team_22/Giiku_Camp_vol_12_team_22.py
+++ Giiku_Camp_vol_12_team_22/Giiku_Camp_vol_12_team_22.py
@@ -1,22 +1,27 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+import random
 
 import reflex as rx
-
 from rxconfig import config
 
 
 class State(rx.State):
     generated_name = ''
     def generate_team_name(self):
+        if random.randint(0, 3) == 0:
+            self.generated_name = self._choice(1) + self._choice(2)
+        else:
+            self.generated_name = self._choice(0) + self._choice(1) + self._choice(2)
+        print(self.generated_name) ## デバッグ用
+    
+    def _choice(self, index):
         pass
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="top-right"),
     )
 
 
 app = rx.App()
-app.add_page(index)S
+app.add_page(index)
